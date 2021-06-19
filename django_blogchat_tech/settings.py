22b593d540751107e0258d93c034b1fc93d789af
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-06-19 00:30:04 +0000

    Fixed SECRET_KEY in setting.py

--- django_blogchat_tech/settings.py
+++ django_blogchat_tech/settings.py
@@ -15,9 +15,9 @@ import dj_database_url
 from pathlib import Path
 from os import path
 
-## Add recommended by anna_ci
+# Add recommended by anna_ci
 if path.exists("env.py"):
-  import env 
+    import env
 
 if os.environ.get('DEVELOPMENT'):
     development = True
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(SECRET_KEY,'')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
