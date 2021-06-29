eae8fe9c3a96aa2b1cc17acc28c771923ff9b8cc
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-06-29 18:50:02 -0400

    Fix DEBUG = 'DEVELOPMENT' in os.environ

--- django_blogchat_tech/settings.py
+++ django_blogchat_tech/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-DEBUG = development
+DEBUG = 'DEVELOPMENT' in os.environ
 
 if development:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
