2c979163004814abd0f9425e6ee97003da76181d
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-07-18 17:25:24 -0400

    Add tests for profile models

--- profiles/test_models.py
+++ profiles/test_models.py
@@ -1,5 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
 from django.test import TestCase
-from .models import UserProfile, User
+from .models import UserProfile
 
 # Create your tests here.
 
