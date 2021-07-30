c4051332dfebc6183f63b4a2999e20fee45853cd
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-07-30 17:00:47 -0400

    Create list donations view and add capture donation info function

--- donate/views.py
+++ donate/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
-# from .models import Donation
+from .models import Donation
+from django.contrib.auth import authenticate
+
 import datetime
 import stripe
 
@@ -31,6 +33,14 @@ def charge(request):
         
         amount = int(request.POST['amount'])
 
+        donation = Donation.objects.create(
+            donor_name = request.POST['username'],
+            donor_email = request.POST['email'],
+            donate_date = datetime.date.today(),
+            amount = request.POST['amount'],
+            donated = True
+        )
+
         customer = stripe.Customer.create(
             email=request.POST['email'],
 	        name=request.POST['username'],
@@ -43,14 +53,6 @@ def charge(request):
             currency='usd',
             description = 'Donation'
         )
-
-        # donation.objects.create(
-        #     donor = request.POST['username'],
-        #     donate_date = datetime.date.today(),
-        #     amount = amount,
-        #     donated = True
-        # )
-       
     return redirect(reverse('success', args=[amount]))
 
 
@@ -59,20 +61,21 @@ def successMsg(request, args):
     amount = args
     return render(request, 'donate/success.html', {'amount':amount})
 
+
 def cancelMsg(request):
     """A view to notify donation has been cancelled """
     return render(request, 'donate/cancel.html')
 
 
-# def donations(request):
-#     """ A view to list donations """
-#     # Consider restricting by user
-#     donations = Donation.objects.all()
+def donations(request):
+    """ A view to list donations """
+    # Consider restricting by user
+    donations = Donation.objects.all()
 
-#     context = {
-#         'donations' : donations
-#     }
+    context = {
+        'donations' : donations
+    }
 
-#     return render(request, 'donate/donations.html', context)
+    return render(request, 'donate/donations.html', context)
 
 
