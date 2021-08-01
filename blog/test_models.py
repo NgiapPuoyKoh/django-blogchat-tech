6b543035974347c162c9399fe765febd5f1a5714
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-08-01 01:24:24 -0400

    Fix blog test_models.py, execute and pass all tests

--- blog/test_models.py
+++ blog/test_models.py
@@ -14,9 +14,10 @@ class TestModels(TestCase):
         # Set up non-modified objects used by all test methods
         user = User.objects.create(username='Author1', password='Author1Pass', email = 'Author1@email.com')
         topic = Topic.objects.create(name='django', friendly_name= 'Django')
-        post1 = Post.objects.create(
+        Post.objects.create(
             title = 'How to test model',
-            publish_date = datetime.datetime.now(),
+            slug = 'How-to-test-model',
+            publish = timezone.now(),
             author = user,
             content = 'Setup test data using <model>.objects.create',
             status = 'Draft',
@@ -25,24 +26,28 @@ class TestModels(TestCase):
     
 
     def test_post_is_assigned_slug_on_create(self):
-        self.assertEquals(post1.slug, 'How to test model')
+        post1 = Post.objects.get(id=1)
+        self.assertEquals(post1.slug, 'How-to-test-model')
 
 
     def test_status_defaults_to_draft(self):
-       self.assertEquals(post1.status, 'draft')
+        post1 = Post.objects.get(id=1)
+        self.assertEquals(post1.status, 'Draft')
 
 
     def test_title_label(self):
+        post1 = Post.objects.get(id=1)
         field_label = post1._meta.get_field('title').verbose_name
         self.assertEqual(field_label, 'title')
 
 
     def test_title_max_length(self):
-        max_length = self.post1._meta.get_field('title').max_length
+        post1 = Post.objects.get(id=1)
+        max_length = post1._meta.get_field('title').max_length
         self.assertEqual(max_length, 254)
     
     
     # def test_get_absolute_url(self):
-    #     post = Post.objects.get(id=1)
+    #     post1 = Post.objects.get(id=1)
     #     # This will also fail if the urlconf is not defined.
-    #     self.assertEqual(post.get_absolute_url(), '/blog/1/')
+    #     self.assertEqual(post1.get_absolute_url(), '/blog/How-to-test-model/')
