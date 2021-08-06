8b6976a311a14ebe84016e0660229745124036e8
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-08-06 07:34:42 -0400

    Create PostCreateView, post_form.html and post-create url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+# from .views import PostListViews
+# from .views import PostDetailView
+from .views import PostCreateView
+# from .views import post_add
 from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
     path('posts/', views.all_posts, name='posts'),
-    # path('<post_id>', views.post_detail, name='post_detail')
-    path('<slug:post>/', views.post_detail, name='post_detail')
+    # path('<post_id>/', views.post_detail, name='post_detail')
+    path('<slug:post>/', views.post_detail, name='post_detail'),
+    # path('post/', PostDetailView.as_view(), name='post_detail')
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
+
+#<app>/<model>_<viewtype>.html
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.conf.urls import url
+from django.shortcuts import render, get_object_or_404, redirect
 # from blog.models import Post 
 from .models import Post
+from django.views.generic.edit import CreateView
+from django.urls import reverse
 
-# from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -35,13 +37,33 @@ def post_detail(request, post):
 
     return render(request, 'blog/post_detail.html', context)
 
+
+class PostCreateView(CreateView):
+    model = Post
+
+    fields = [
+        'title',
+        'excerpt',
+        'content',
+        'topic',
+        'status',
+        'slug'
+    ]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        # form.instance.slug = slugify(self.post.title)
+        return super().form_valid(form)
+
+    # def get_success_url(self):
+    #     return reverse('post_detail', kwargs={'slug': self.post})
+
 # def add_post(request, user):
 #     """ A function to create a blog post and 
-#     render the add_blog page """
-
+#     render the add_blog page "
     
 # def PostDetailView(DetailView):
 #     model = Post
 
 
-    #<app>/<model>_<viewtype>.html
+#<app>/<model>_<viewtype>.html
