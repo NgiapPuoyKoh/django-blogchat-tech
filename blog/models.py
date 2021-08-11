9784150690284803a17d1ddac3017fba5684c314
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-08-11 15:21:26 -0400

    Create blog app Comment model

--- blog/models.py
+++ blog/models.py
@@ -105,3 +105,21 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
     # instance.slug = slug
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name ="comments")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    content = models.TextField()
+    publish = models.DateTimeField(auto_now_add = True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("publish",)
+
+    def __str__(self):
+        return f"Comment by {self.name}"
+    
+
+
