d48c73e2e028fb4bbb65290182871f6313e3d19d
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-08-24 12:49:24 -0400

    Add label and bootstrap class to style PostSearchForm

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Comment, Post
 
+
 class NewCommentForm(forms.ModelForm):
     """ For for use to add a comment to blog post """
     class Meta:
@@ -12,6 +13,15 @@ class NewCommentForm(forms.ModelForm):
             "content": forms.Textarea(attrs={"class": "form-control"}),
         }
 
+
 class PostSearchForm(forms.Form):
+    """ Form Text input for post search """
     q = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['q'].label = 'Search For'
+        self.fields['q'].widget.attrs.update(
+            {'class': 'form-control'})
+        
     
