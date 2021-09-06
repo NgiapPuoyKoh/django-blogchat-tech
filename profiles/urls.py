f042e77c4a014f7714fa7a093bdd893f7a81e31f
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-09-06 14:57:31 -0400

    Edit profiles url add end / to paths

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<str:user>/', views.user_profile, name='user_profile'),
-    path('edit_profile/<str:user>', views.edit_profile, name='edit_profile'),
-    path('delete_profile/<str:user>', views.delete_profile, name='delete_profile'),
+    path('edit_profile/<str:user>/', views.edit_profile, name='edit_profile'),
+    path('delete_profile/<str:user>/', views.delete_profile, name='delete_profile'),
 ]
