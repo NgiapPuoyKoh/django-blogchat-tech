04a856eebc041e1f37416d393bd541a1f645f815
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-09-11 11:40:54 -0400

    Edit blog test PEP8 compliance

--- blog/test_models.py
+++ blog/test_models.py
@@ -7,21 +7,25 @@ from .models import Post, Topic, Comment
 from django.utils import timezone
 import datetime
 
+
 class TestModels(TestCase):
 
     @classmethod
     def setUp(cls):
         # Set up non-modified objects used by all test methods
-        user = User.objects.create(username='Author1', password='Author1Pass', email = 'Author1@email.com')
-        topic = Topic.objects.create(name='django', friendly_name= 'Django')
+        user = User.objects.create(
+            username='Author1', password='Author1Pass',
+            email='Author1@email.com')
+        topic = Topic.objects.create(
+            name='django', friendly_name='Django')
         Post.objects.create(
-            title = 'How to test model',
-            slug = 'How-to-test-model',
-            publish = timezone.now(),
-            author = user,
-            content = 'Setup test data using <model>.objects.create',
-            status = 'Draft',
-            topic = topic
+            title='How to test model',
+            slug='How-to-test-model',
+            publish=timezone.now(),
+            author=user,
+            content='Setup test data using <model>.objects.create',
+            status='Draft',
+            topic=topic
         )
 
         # Comment.objects.create(
@@ -31,29 +35,25 @@ class TestModels(TestCase):
         #     content = 'This is a comment on the post',
         #     published = timezone.now()
         # )
-    
+
     def test_post_is_assigned_slug_on_create(self):
         post1 = Post.objects.get(id=1)
         self.assertEquals(post1.slug, 'How-to-test-model')
 
-
     def test_status_defaults_to_draft(self):
         post1 = Post.objects.get(id=1)
         self.assertEquals(post1.status, 'Draft')
-
 
     def test_title_label(self):
         post1 = Post.objects.get(id=1)
         field_label = post1._meta.get_field('title').verbose_name
         self.assertEqual(field_label, 'title')
 
-
     def test_title_max_length(self):
         post1 = Post.objects.get(id=1)
         max_length = post1._meta.get_field('title').max_length
         self.assertEqual(max_length, 254)
-    
-    
+
     def test_get_absolute_url(self):
         post1 = Post.objects.get(id=1)
         # This will also fail if the urlconf is not defined.
@@ -63,31 +63,32 @@ class TestModels(TestCase):
 class CommentModelTest(TestCase):
     @classmethod
     def setUp(cls):
-        user = User.objects.create(username='Author1', password='Author1Pass', email = 'Author1@email.com')
-        topic = Topic.objects.create(name='django', friendly_name= 'Django')
+        user = User.objects.create(
+            username='Author1', password='Author1Pass',
+            email='Author1@email.com')
+        topic = Topic.objects.create(
+            name='django', friendly_name='Django')
         Post.objects.create(
-            title = 'How to test model',
-            slug = 'How-to-test-model',
-            publish = timezone.now(),
-            author = user,
-            content = 'Setup test data using <model>.objects.create',
-            status = 'Draft',
-            topic = topic
+            title='How to test model',
+            slug='How-to-test-model',
+            publish=timezone.now(),
+            author=user,
+            content='Setup test data using <model>.objects.create',
+            status='Draft',
+            topic=topic
         )
 
         Comment.objects.create(
-            post = Post.objects.get(id=1),
-            name = 'Commentor',
-            email = 'commentor@email.com',
-            content = 'This is a comment on the post',
-            publish = timezone.now()
+            post=Post.objects.get(id=1),
+            name='Commentor',
+            email='commentor@email.com',
+            content='This is a comment on the post',
+            publish=timezone.now()
         )
-
 
     def test_comment_status_defaults_to_true(self):
         comment1 = Comment.objects.get(id=1)
         self.assertEquals(comment1.status, True)
-
 
     def test_comment_name_label(self):
         comment1 = Comment.objects.get(id=1)
--- blog/test_views.py
+++ blog/test_views.py
@@ -6,26 +6,27 @@ from django.utils import timezone
 from django.utils.text import slugify
 from blog.forms import PostSearchForm
 
-
-
 # Create your tests here.
+
+
 class TestViews(TestCase):
 
     @classmethod
     def setUp(cls):
         # Set up non-modified objects used by all test methods
-        user = User.objects.create(username='Author1', password='Author1Pass', email = 'Author1@email.com')
-        topic = Topic.objects.create(name='django', friendly_name= 'Django')
+        user = User.objects.create(
+            username='Author1', password='Author1Pass',
+            email='Author1@email.com')
+        topic = Topic.objects.create(name='django', friendly_name='Django')
         post1 = Post.objects.create(
-            title = 'How to test model',
-            slug = 'Howtotestmodel',
-            publish = timezone.now(),
-            author = user,
-            content = 'Setup test data using <model>.objects.create',
-            status = 'Draft',
-            topic = topic
+            title='How to test model',
+            slug='Howtotestmodel',
+            publish=timezone.now(),
+            author=user,
+            content='Setup test data using <model>.objects.create',
+            status='Draft',
+            topic=topic
         )
-    
 
     def test_fail_blog_page(self):
         """ TDD Fail Render blog Page """
@@ -33,14 +34,12 @@ class TestViews(TestCase):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
 
-
     def test_blog_home_page(self):
         """ Test blog page renders blog.html """
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/blog.html')
 
-    
     def test_blog_posts_list(self):
         """Test blog posts list renders posts.html"""
         posts = Post.objects.all()
@@ -48,20 +47,17 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/posts.html')
 
-
     def test_posts_search(self):
         """Test blog posts search renders posts.html"""
         response = self.client.get(reverse('posts_search'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/search.html')
 
-
     # def test_posts_search_get(self):
     #     """Test blog posts search get renders posts.html"""
     #     response = self.client.get(reverse('posts_search'))
     #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'blog/search.html')
-
+    #     self.assertTemplateUsed(response, 'blog/search.html')\
 
     # def test_blog_post_detail(self):
     #     """Test post detail renders post_detail.html """
