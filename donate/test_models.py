25ece9d8a166f53406e8f958fa3b396cb1419689
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-09-12 07:56:04 -0400

    Edit donate model test PEP8 compliance

--- donate/test_models.py
+++ donate/test_models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.test import TestCase
 from .models import Donation
+from django.utils import timezone
 import datetime
+
 
 class TestModels(TestCase):
 
     def test_donated_defaults_to_true(self):
-       donation = Donation.objects.create(donor_name='Tester', donate_date=datetime.date.today() ) 
-       self.assertTrue(donation.donated)
+        donation = Donation.objects.create(
+            donor_name='Tester', donate_date=timezone.now())
+        self.assertTrue(donation.donated)
